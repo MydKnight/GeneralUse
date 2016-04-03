4a574a4361766dd82b2c4ce2fdb5f664a9b37a27
madsens <shiloh.madsen@gmail.com>
2016-04-03 18:42:03 -0400

    Initial Commit of General Use Codes

---
+++ Furnace.py
@@ -0,0 +1,38 @@
+__author__ = 'madsens'
+
+import Logging
+import os
+import Movies
+import time
+import Lights
+
+# This code runs the Furnace
+Lights.setup()
+
+Logging.PowerLog()
+print 'Starting'
+Movies.StartLoop('/home/pi/NYE2015/Assets/Furnace')
+
+while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
+    #Logging.HeartBeat()
+    n = raw_input("Scanned ID: ")
+    Lights.showColor("gold")
+    if n == "STOP":
+        Movies.StopLoop()
+        Lights.cleanup()
+        print "Stopping."
+        break  # stops the loop
+    else :
+        # On Input, Disable Reader
+        os.system("/home/pi/NYE2015/Scripts/disableRFID.sh")
+        Lights.showColor("red")
+        time.sleep(2)
+        Lights.showColor("none")
+        print "Playing."
+        Logging.LogAccess(n)
+        Movies.PlayMovie()
+
+        time.sleep(60)
+	Lights.showColor("gold")
+        # Reenable reader.
+        os.system("/home/pi/NYE2015/Scripts/enableRFID.sh")
