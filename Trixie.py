70705743c9e9bb0c63ef4ff60e7901a808a2cde1
madsens <shiloh.madsen@gmail.com>
2016-05-07 17:40:50 -0400

    Trying to handle for an EOF error

--- Trixie.py
+++ Trixie.py
@@ -15,32 +15,35 @@ dbConn.logBoot()
 lastScan = 0
 previousFile = ""
 Lights.setup()
-os.system("/home/pi/Halloween2015/Scripts/enableRFID.sh")
+os.system("/home/pi/Scripts/enableRFID.sh")
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
-    n = raw_input("Scanned ID: ")
-    currentScan = time.time()
-    if n == "STOP":
-        break  # stops the loop
-    else :
-        dbConn.logAccess(n)
+    try:
+        n = raw_input("Scanned ID: ")
+        currentScan = time.time()
+        if n == "STOP":
+            break  # stops the loop
+        else :
+            dbConn.logAccess(n)
 
-        #Disable RFID
-        os.system("/home/pi/Scripts/disableRFID.sh")
+            #Disable RFID
+            os.system("/home/pi/Scripts/disableRFID.sh")
 
-        #Trigger GPIO Pins. Trixie just uses pin 13
-        Lights.on([13])
+            #Trigger GPIO Pins. Trixie just uses pin 13
+            Lights.on([13])
 
-        time.sleep(1)
+            time.sleep(1)
 
-        #Play Creepy Laugh
-        os.system('mpg321 /home/pi/Assets/TrixieBlinkChimes.mp3 -q')
+            #Play Creepy Laugh
+            os.system('mpg321 /home/pi/Assets/TrixieBlinkChimes.mp3 -q')
 
-        #Trixie GPIO Off
-        Lights.off([13])
+            #Trixie GPIO Off
+            Lights.off([13])
 
-        #Log Activation of PI
-        #Logging.LogAudioAccess(n, previousFile)
+            #Log Activation of PI
+            #Logging.LogAudioAccess(n, previousFile)
 
-        #Reenable RFID
-        os.system("/home/pi/Scripts/enableRFID.sh")
+            #Reenable RFID
+            os.system("/home/pi/Scripts/enableRFID.sh")
+    except (EOFError):
+        pass
